e6c56ce7d680f49f344f7e391fc5a4877ba0baed
jhonnygoms <jhonnygoms@gmail.com>
2025-10-25 13:51:22 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Quicken Features'
+project = 'Quicken Available Features'
 copyright = '2025'
 author = 'Dalton Brown'
 
@@ -21,3 +21,4 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
